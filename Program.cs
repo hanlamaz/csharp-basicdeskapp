b60fbde21c7779e0bfc871b3bad8b76f0e6d09ab
hsynkas <hanlamaz13@posta.pau.edu.tr>
2015-03-25 15:45:13 +0200

    Added .cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Decks mydesk = new Decks();
+            mydesk.View();
+            Console.WriteLine("");
+            mydesk.Shuffle();
+            mydesk.View();
+            Console.WriteLine("");
+            Console.WriteLine(mydesk.SelectUpperCard().ToString());
+        }
+    }
+}
