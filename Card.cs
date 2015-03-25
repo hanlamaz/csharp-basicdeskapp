e4412d9ec31e3523a85435da41c7335c40504ae2
hsynkas <hanlamaz13@posta.pau.edu.tr>
2015-03-25 15:44:55 +0200

    Added .cs

---
+++ Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Card
+    {
+        string suit; // Hearts,Tiles(Diamonds)Clovers(Clubs),Pikes(Spades)
+        string face; // 1,2,3,4...
+        public Card(string face ,string suit)
+        {
+            this.face = face;
+            this.suit = suit;
+        }
+        public string ToString()
+        {
+            return suit + " " + face;
+        }
+
+    }
+}
